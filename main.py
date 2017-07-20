7d083c6d0f1c0e1b46d3659b4a521d55cba673f0
zeronofreya <zeronofreya@yahoo.com>
2017-07-21 00:16:07 +0800

    0

--- main.py
+++ main.py
@@ -4,29 +4,45 @@ import sciter
 import ctypes
 import time
 import win32con
-SendMessage = ctypes.windll.user32.SendMessageW
-from concurrent.futures import ProcessPoolExecutor as Pool
+import requests
+
+from multiprocessing import Queue, Process
+
 ctypes.windll.user32.SetProcessDPIAware(2)
+SendMessage = ctypes.windll.user32.SendMessageW
 
 myWM_USER = win32con.WM_USER+10
-
-def run_proc(name, hwnd):
-    time.sleep(2)
+path = 'https://68.media.tumblr.com/c0c18a1f04a764b80d9c005b7cbe95f5/tumblr_oteae2YKrt1rzvykfo1_250.jpg'
+proxies = {
+    "http": "127.0.0.1:50175",
+    "https": "127.0.0.1:50175"
+}
+file_path = 'r:/a.jpg'
+def run(qq, hwnd):
+    req = requests.get( path, proxies=proxies )
+    with open(file_path, 'wb') as fh:
+        for chunk in req.iter_content(chunk_size=1024):
+            fh.write(chunk)
+    qq.put([{
+        'p':file_path
+    }]) #子进程放入数据
     SendMessage(hwnd, myWM_USER, 0, 0)
 
 class Frame(sciter.Window):
     def __init__(self):
         super().__init__(ismain=True, uni_theme=False, debug=True)
         self.set_dispatch_options(enable=True, require_attribute=False)
-        self.pool = Pool(max_workers=1)
+        # self.pool = Pool(max_workers=1)
+        self.q = Queue()
 
     def on_message(self, hwnd, msg, wparam, lparam):
         if msg == myWM_USER:
-            print("ok")
+            print(self.q.get()) #父进程中获取,打印
+            # print("ok")
         # print(hwnd, msg, wparam, lparam)
         pass
     def test(self):
-        self.pool.submit(run_proc, 'ttt', self.hwnd)
+        Process(target=run, args=(self.q, self.hwnd)).start()
 
 if __name__ == '__main__':
     frame = Frame()
